f357cef63b0fa8ff02067085196354cb729df3c7
CodePlexLabs <alok.kumar.sahoo@hotmail.com>
2025-06-03 19:33:26 +0530

    feat: integrate HTTP client and fetch users on app initialization
    - Updated AppComponent to implement OnInit and fetch user data from the API on initialization.
    - Added NgFor to the imports for rendering user data in the template.
    - Configured the application to provide the HTTP client in app.config.ts.
    - Added SSL certificate and private key for local development.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dating-app';
+  http = inject(HttpClient);
+  users: any;
+  ngOnInit(): void {
+    this.http.get('https://localhost:7036/api/user').subscribe({
+      next: response => {
+        this.users = response;
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+  }
 }
